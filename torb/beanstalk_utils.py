9d05715ad51447e1135092c946e977a8262e90a4
j1z0 <jeremy@softworks.com.my>
2017-11-12 20:41:02 -0500

    getting stuff working in new repo

--- torb/beanstalk_utils.py
+++ torb/beanstalk_utils.py
@@ -89,11 +89,11 @@ def whodaman():
     '''
     magic_cname = 'fourfront-webprod.9wzadzju3p.us-east-1.elasticbeanstalk.com'
 
-    import pdb
-    pdb.set_trace()
     client = boto3.client('elasticbeanstalk')
     res = client.describe_environments(ApplicationName="4dn-web")
+    logger.warn(res)
     for env in res['Environments']:
+        logger.warn(env)
         if env.get('CNAME') == magic_cname:
             # we found data
             return env
