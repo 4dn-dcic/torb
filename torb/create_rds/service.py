69ef305622637490f7abb42962ebd60b964eef3c
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-04-30 14:52:06 -0400

    Update create_rds and snapshot_rds

--- torb/create_rds/service.py
+++ torb/create_rds/service.py
@@ -2,6 +2,7 @@
 import os
 import logging
 from dcicutils import beanstalk_utils as bs
+from torb.utils import powerup, get_default
 
 
 logging.basicConfig()
@@ -13,23 +14,31 @@ def get_default(data, key):
     return data.get(key, os.environ.get(key, None))
 
 
+@powerup('create_rds')
 def handler(event, context):
+    """
+    Run beanstalk_utils.create_db_from_snapshot, which will create a DB
+    from a given snapshot. The `db_id` from the event is used as the DB instance
+    name, and if a current db already exists with this name, it will attempt to
+    delete it first. For this reason, do NOT proceed when `db_id` is
+    fourfront-webprod or fourfront-webprod2.
+
+    The `snapshot_arn` from the event is the ARN of the RDS snapshot to restore.
+    """
     # get data
-    item_id = get_default(event, 'id')
+    item_id = get_default(event, 'db_id')
+    snapshot_arn = get_default(event, 'snapshot_arn')
     dry_run = get_default(event, 'dry_run')
 
-    retval = {"type": "create_rds",
-              "id": item_id,
-              "dry_run": dry_run
-              }
+    retval = {"type": "create_rds", "id": item_id, "dry_run": dry_run}
 
-    if item_id == "fourfront-webprod" or item_id.startswith('fourfront-webprod'):
-        print("not deleteing production database")
+    if 'fourfront-webprod' in id:
+        print("not deleting production database")
         retval['info'] = 'skipping prod database'
         return retval
 
-    if not dry_run:
-        arn = bs.create_db_from_snapshot(item_id)
+    if not dry_run and (item_id and snapshot_arn):
+        arn = bs.create_db_from_snapshot(item_id, snapshot_arn)
         if arn == "Deleting":
             raise bs.WaitingForBoto3("Waiting for RDS to delete")
 
--- torb/snapshot_rds/service.py
+++ torb/snapshot_rds/service.py
@@ -2,7 +2,7 @@
 import os
 import logging
 from dcicutils import beanstalk_utils as bs
-from torb.utils import powerup
+from torb.utils import powerup, get_default
 
 
 logging.basicConfig()
@@ -10,29 +10,27 @@ logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
 
 
-def get_default(data, key):
-    return data.get(key, os.environ.get(key, None))
-
-
 @powerup('snapshot_rds')
 def handler(event, context):
+    """
+    Run beanstalk_utils.create_db_snapshot, which will snapshot the RDS instance
+    with `db_id` and use `snapshot_id` as the snapshot identifier. If a current
+    snapshot already exists with this id, it will delete it.
+    """
     # get data
-    source = get_default(event, 'source_env')
-    dest = get_default(event, 'dest_env')
+    db_id = get_default(event, 'db_id')
+    snapshot_id = get_default(event, 'snapshot_id')
     dry_run = get_default(event, 'dry_run')
 
     if dry_run:
-        res = "Dry Run - would have ran create_db_snapshot(%s, %s)" % (source, dest)
-        dbid = "dry_run"
+        res = "Dry Run - would have ran create_db_snapshot(%s, %s)" % (db_id, snapshot_id)
+        ret_dbid = "dry_run"
     else:
-        res = bs.create_db_snapshot(source, dest)
+        res = bs.create_db_snapshot(db_id, snapshot_id)
         if res == "Deleting":
                 raise bs.WaitingForBoto3("Waiting for RDS to delete")
 
-        dbid = res['DBSnapshot']['DBSnapshotIdentifier']
+        ret_dbid = res['DBSnapshot']['DBSnapshotIdentifier']
     logger.info(res)
 
-    return {'type': 'snapshot',
-            'id': dbid,
-            'dry_run': dry_run,
-            }
+    return {'type': 'snapshot', 'id': ret_dbid, 'dry_run': dry_run}
