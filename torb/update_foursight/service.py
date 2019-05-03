2422485506455ac8d99f871d05bae1bb99ddce4d
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-05-03 12:43:24 -0400

    Refactor torb/update_foursight

--- torb/update_foursight/service.py
+++ torb/update_foursight/service.py
@@ -2,7 +2,7 @@
 import os
 import logging
 from dcicutils import beanstalk_utils as bs
-from torb.utils import powerup
+from torb.utils import powerup, get_default
 
 
 logging.basicConfig()
@@ -10,27 +10,35 @@ logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
 
 
-def get_default(data, key, default=None):
-    return data.get(key, os.environ.get(key, default))
-
-
 @powerup('update_foursight')
 def handler(event, context):
+    """
+    Use a PUT request to create a Foursight environment for the given
+    ElasticBeanstalk environment name and update the event JSON with the
+    resulting environment name on success. Also attempt to log to Foursight
+    using with a PUT request to checks endpoint.
+
+    `dest_env` in the event JSON determines the ElasticBeanstalk environment.
+
+    Sets the "event" key of the event JSON to the name of created FS environment.
+    """
     dest_env = get_default(event, 'dest_env')
     dry_run = get_default(event, 'dry_run')
 
     if dry_run:
         return event
-
-    foursight = bs.create_foursight_auto(dest_env)
-
-    event['foursight'] = foursight
     try:
+        foursight = bs.create_foursight_auto(dest_env)
+    except Exception as exc:
+        logger.warning('Error in update_foursight on bs.create_foursight_auto: %s' % exc)
+    else:
+        event['foursight'] = foursight
+    try:
+        # if staging, update the staging build check
         if foursight.get('fs_url') == 'staging':
-            # if staging, side effect and update staging_build
-            bs.log_to_foursight(event, '', status="PASS",
-                                full_output="Updating foursight")
-    except AttributeError:
-        pass
+            check_fields = {'status': 'PASS', 'full_output': 'Updating foursight'}
+            bs.log_to_foursight(event, 'update_foursight', check_fields)
+    except Exception as exc:
+        logger.warning('Error in update_foursight on bs.log_to_foursight: %s' % exc)
 
     return event
